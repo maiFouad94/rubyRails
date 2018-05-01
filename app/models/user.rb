5075f85886d019e4aa46b166ac0c03297caaee5c
maiFouad94 <mai_elrefaey@yahoo.com>
2018-05-01 14:42:11 +0200

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+validates :name, presence: true
+validates :name, uniqueness: true
+
+enum gender: {male: 0,female: 1}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+
+
+  # match '/users', 'users#index'
+  # match '/users/:id', 'users#show', :get
+  # post '/users' ,'users#create', as: give_me_users_create
+  # get '/users/new' , 'users#new', as: direct_me_to_new_user_path  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
