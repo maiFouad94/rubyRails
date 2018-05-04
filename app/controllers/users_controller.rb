90f3a8f506ba1688e6310f0e181b3a5c670b6575
maiFouad94 <mai_elrefaey@yahoo.com>
2018-05-04 20:57:41 +0200

    get male users only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def male
+    @user = User.where(gender: 'male')
+  
     render :male
   end
   # GET /users
